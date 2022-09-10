a480079f786b0c6222740f4a73e1c4865fbdc6e7
Sam <96780161+vyse9@users.noreply.github.com>
2022-09-10 06:21:27 +0530

    Update admin.py

--- main_repo-master/main_repo-master/onlinecourse/admin.py
+++ main_repo-master/main_repo-master/onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission, Enrollment
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -8,6 +8,14 @@ from .models import Course, Lesson, Instructor, Learner
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 # Register your models here.
@@ -20,6 +28,14 @@ class CourseAdmin(admin.ModelAdmin):
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
+    # inlines = [QuestionInline]
+
+class QuestionAdmin(admin.ModelAdmin):
+     inlines = [ChoiceInline]
+     list_display = ['question_text', 'course']
+
+class ChoiceAdmin(admin.ModelAdmin):
+   list_display = ['choice_text', 'question']
 
 
 # <HINT> Register Question and Choice models here
@@ -28,3 +44,9 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Enrollment)
+admin.site.register(Submission)
+
